342c5b487985e8fe3d9bcb07a0dd81d6ab5a1d66
Satyam Tripathi <st17931@gmail.com>
2023-12-04 06:31:24 +0530

    Second Commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,50 @@
 import asyncHandler from "express-async-handler"
 
 //@desc  Auth user/set token
-//@route /api/user/auth
+//@route POST /api/users/auth
 //@access public
 
 const authUser =  asyncHandler(async(req,res)=>{
     res.status(200).json({ message: 'Auth User'});
 })
 
+//@desc  Register a new user
+//@route POST /api/users
+//@access Public
 
-export {
-    authUser
-};
+const registerUser =  asyncHandler(async(req,res)=>{
+    res.status(200).json({ message: 'Register User'});
+})
+
+//@desc  Logout user
+//@route POST /api/user/logout
+//@access public
+
+const logoutUser =  asyncHandler(async(req,res)=>{
+    res.status(200).json({ message: 'Logout User'});
+})
+
+//@desc  get user profile
+//@route POST /api/users/profile
+//@access private
+const getUserProfile =  asyncHandler(async(req,res)=>{
+    res.status(200).json({ message: 'Get User Profile'});
+})
+
+//@desc  update user profile
+//@route PUT /api/user/profile
+//@access public
+
+const updateUserProfile =  asyncHandler(async(req,res)=>{
+    res.status(200).json({ message: 'Update User Profile'});
+})
+
+
+export{
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+}
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile, logoutUser, registerUser, updateUserProfile } from "../controllers/userController.js";
 
 const Router = express.Router();
 
 Router.post("/auth",authUser);
+Router.post("/register",registerUser);
+Router.post("/logout",logoutUser);
+Router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default Router;
